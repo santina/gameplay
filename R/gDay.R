52df4cd04f1a22698acc308d59efb0d0242c67b5
Santina Lin <santina424@gmail.com>
2014-11-19 14:49:05 -0800

    modified excpetion handling

--- R/gDay.R
+++ R/gDay.R
@@ -8,29 +8,43 @@
 #' Now here's the input
 #'
 #' @param team The name of your team or the city of the team.
-#' @return string Name of the channel to watch
+#' @return string Name of the channel to watch. Return "information not available for this date"
+#' if the NHL server cannot provide information for the given date.
+#' Returns "not available" if the team is #' not playing on the current day,
+#' "undecided or not available" if the team is playing but the channel name
+#' is not specified on NHL
 #' @note case in \code{team} is ignored it will check for Canucks because this package
 #' was written by someone in Vancouver
+#'
 #' @export
 #' @examples
 #' gday() # check for "Canucks"
-#' gday("Bruins")
+
 
 #source("checkDate.R")
 #source("checkNHLConnection.R")
-gday <- function(team="canucks") {
-	source("checkDate.R")
-	source("checkNHLConnection.R")
+gday <- function(date = Sys.Date(), team="canucks") {
+
+	# check date, throw erros and stop the function if it's not correct
+	checkDate(date)
+
+
 	if(!checkNHLConnection()){
 		stop("Cannot connect to NHL server :(  Try checking your internet, too.")
 	}
-	url <- paste0("http://live.nhle.com/GameData/GCScoreboard/", Sys.Date(), ".jsonp")
-    #"http://live.nhle.com/GameData/GCScoreboard/2014-11-10.jsonp"
-  good <- grepl(team, getURL(url), ignore.case=TRUE) # relies on RCurl package
 
-  if(!good){
-  	stop("sorry " + team +  "isn't playing today. Wait a few more days, or you can use" +
-  			 	"score(date, team) to see if your team is playing on that date")
+
+	url <- paste0("http://live.nhle.com/GameData/GCScoreboard/", date, ".jsonp")
+    #"http://live.nhle.com/GameData/GCScoreboard/2014-11-10.jsonp"
+  good <- grepl(team, RCurl::getURL(url), ignore.case=TRUE) # relies on RCurl package
+
+	unavailable <- grepl("404 Not Found", RCurl::getURL(url))
+
+	if(unavailable){
+		channel <- "information not available for this date"
+	}
+  else if(!good){
+  	channel <- "not playing"
   }
   else{
 
@@ -38,7 +52,10 @@ gday <- function(team="canucks") {
   	# this parsing code was taken from the homework instruction page
   	json <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)', "", raw, perl = TRUE)
 
-  	# for whatever reason the server doesn't return any info even though it's running
+  	# for whatever reason the server doesn't return any info
+  	# or changes its JSON format
+
+  	data <- jsonlite::fromJSON(json)$games
   	tryCatch({
   		data <- jsonlite::fromJSON(json)$games
   		},
@@ -66,7 +83,7 @@ gday <- function(team="canucks") {
   	}
   }
 	if (nchar(channel) == 0){
-		channel  <- "undecided"
+		channel  <- "undecided or not available"
 	}
 
 	channel
--- R/score.R
+++ R/score.R
@@ -7,13 +7,15 @@
 #' as available on live.nhle.com.
 #'
 #' @param date The scores of the date you want to search for. format: YYYY-MM-DD
-#' @param team The team of your interest. If it's playing as indicated by gday(),
-#' the function will return the score information of both the team and its rival team.
+#' @param team The team of your interest, can the team's city name, too.
 #' @return data.frame If connection to live.nhle.com is successful and
 #' that the date is the right format
 #' @note When \code{date} is ignored it will check for \code{Sys.Date()},
 #' and if \code{team} is ignored, the function will return all the teams playing on
 #' the given day.
+#' If the input team playing, the function will return the score information of both the team and its rival team.
+#' If the input team is not playing, the function prints "The team isn't playing today"
+#' and returns a data frame of all the games going on today.
 #' The function also throw errors when date format is not proper or
 #' information are not avaiable for certain date.
 #' \code{team} can be either the name of the team or the city names
@@ -26,8 +28,7 @@
 
 
 score <- function(date = Sys.Date(), team = NULL) {
-	source("checkDate.R")
-	source("checkNHLConnection.R")
+
 	checkDate(date)
 	if(!checkNHLConnection()){
 		stop("Cannot connect to NHL server :(  Try checking your internet, too.")
@@ -35,48 +36,46 @@ score <- function(date = Sys.Date(), team = NULL) {
 
 	url  <- paste0('http://live.nhle.com/GameData/GCScoreboard/',
 								 date, '.jsonp')
-
 	raw <- RCurl::getURL(url)
-	# this parsing code was taken from the homework instruction page
-	json <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)', "", raw, perl = TRUE)
 
-		tryCatch({
-			data <- jsonlite::fromJSON(json)$games
-		},
-		error = function(e) {
-			message("It seems like nhl doesn't have the information for that date...")
-			stop()
-			}
-		)
+	unavailable <- grepl("404 Not Found", raw)
+	if(unavailable){
+		result <- "information not available for this date"
+	}
+	else{
+		# this parsing code was taken from the homework instruction page
+		json <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)', "", raw, perl = TRUE)
+		result <- with(data,
+									 data.frame(home = paste(htn, htcommon),
+									 					 away = paste(atn, atcommon),
+									 					 home_score = hts,
+									 					 away_score = ats))
+		if (!identical(team, NULL)){
+			good <- grepl(team, RCurl::getURL(url), ignore.case=TRUE)
+			if(good){
+				# search for both home and away columns in the data frame.
+				index_home <- grep(team, result$home, ignore.case = TRUE)
+				index_away <- grep(team, result$away, ignore.case = TRUE)
 
-	result <- with(data,
-			 data.frame(home = paste(htn, htcommon),
-			 					 away = paste(atn, atcommon),
-			 					 home_score = hts,
-			 					 away_score = ats))
-	if (!identical(team, NULL)){
-		if(gday(team) == TRUE){
-			# search for both home and away columns in the data frame.
-			index_home <- grep(team, result$home, ignore.case = TRUE)
-			index_away <- grep(team, result$away, ignore.case = TRUE)
-
-			if(!identical(index_home, integer(0))){ # if it returns a valid index
+				if(!identical(index_home, integer(0))){ # if it returns a valid index
 					result <- result[index_home, ]
-			}
-			else if(!identical(index_away, integer(0))){
-				result <- result[index_away, ]
-			}
+				}
+				else if(!identical(index_away, integer(0))){
+					result <- result[index_away, ]
+				}
 
-			if (nrow(result) == 1){
-				if(result$home_score == "")#if no score yet
-					print("Team is playing but the scores aren't available yet")
-			}
+				if (nrow(result) == 1){
+					if(result$home_score == "")#if no score yet
+						print("Team is playing but the scores aren't available yet")
+				}
 
-		}
-		else{
-			print("The team isn't playing today")
+			}
+			else{
+				print("The team isn't playing today")
+			}
 		}
 	}
+
 	result
 
 }
